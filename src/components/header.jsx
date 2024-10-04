09c5b3b12cc55fe4ccf3271b000522b9e72a9c07
Ilian Benyahia <isquiiiks@gmail.com>
2024-10-04 09:11:12 +0200

    kasa

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LOGO from "./assets/images/LOGO.png";
+
+const header = ({title}) => {
+    return (
+        <header>
+            <p>{title}</p>
+            <source />
+        </header>
+    );
+};
+
+export default header;
